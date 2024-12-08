cb42960b043a2793729ca81a22265c70d780ec96
Szvetlin Tanyi <svetlintanyi@gmail.com>
2024-12-08 18:16:26 +0100

    fix typescript error

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,7 +26,9 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const deleteInvoiceWithId = async (_formData: FormData) => {
+    await deleteInvoice(id);
+  };
   return (
     <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
